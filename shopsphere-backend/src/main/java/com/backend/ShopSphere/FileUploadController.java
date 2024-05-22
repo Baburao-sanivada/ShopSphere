d82f401d738232fd03d99494903f30fd48cee460
Babu rao sanivada <baburao.sanivada10@gmail.com>
2024-05-23 03:35:58 +0530

    Added Delete Image Functionality

--- shopsphere-backend/src/main/java/com/backend/ShopSphere/FileUploadController.java
+++ shopsphere-backend/src/main/java/com/backend/ShopSphere/FileUploadController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,7 +37,7 @@ public class FileUploadController {
     try {
       String uploadDirectory = "shopsphere-backend/src/main/java/com/backend/ShopSphere/Images";
       String uniqName = imageService.saveImageToStorage(uploadDirectory, img);
-      return ResponseEntity.ok("File uploaded successfully"+uniqName);
+      return ResponseEntity.ok("File uploaded successfully      "+uniqName);
     } catch (IOException e) {
       e.printStackTrace();
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload file");
@@ -44,19 +45,36 @@ public class FileUploadController {
   }
 
   @GetMapping("/getImages/{adsId}")
-    public ResponseEntity<byte[]> getImages(@PathVariable String adsId) throws IOException {
-      
-      try {
-        String imageDirectory = "shopsphere-backend/src/main/java/com/backend/ShopSphere/Images/";
+  public ResponseEntity<byte[]> getImages(@PathVariable String adsId) throws IOException {
+    
+    try {
+      String imageDirectory = "shopsphere-backend/src/main/java/com/backend/ShopSphere/Images/";
 
-        byte[] imageBytes = imageService.getImage(imageDirectory, imageDirectory+adsId);
-        logger.info(imageDirectory+imageBytes);
-        // Respond with the image data and an OK status code
-        return ResponseEntity.ok().body(imageBytes);
-      }catch (Exception e) {
-          // Handle exceptions and provide appropriate error responses
-          return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        }
-    }
+      byte[] imageBytes = imageService.getImage(imageDirectory, adsId);
+      logger.info(imageDirectory+imageBytes);
+      // Respond with the image data and an OK status code
+      return ResponseEntity.ok().body(imageBytes);
+    }catch (Exception e) {
+        // Handle exceptions and provide appropriate error responses
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+      }
+  }
+
+  @DeleteMapping("/delete/{adsId}")
+  public ResponseEntity<String> DeleteImage(@PathVariable String adsId) throws IOException {
+    
+    try {
+      String imageDirectory = "shopsphere-backend/src/main/java/com/backend/ShopSphere/Images/";
+
+      String response = imageService.deleteImage(imageDirectory, adsId);
+      logger.info(response);
+      // Respond with the image data and an OK status code
+      return ResponseEntity.ok().body(response);
+    }catch (Exception e) {
+        // Handle exceptions and provide appropriate error responses
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to delete");
+      }
+  }
+
 }
 
